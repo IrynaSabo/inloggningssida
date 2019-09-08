502ac74829c73a9aab53ef03e2e0bd051d392d63
Förnamn Efternamn <dinepost@example.com>
2019-09-08 21:13:54 +0200

    added the meny with one button

---
+++ script.js
@@ -0,0 +1,6 @@
+var loggainBlock = document.getElementById("loggain_block");
+var userName = document.getElementById("user_name");
+var pswrd = document.getElementById("pswrd");
+const namn = "test";
+const lsnord = "1234";
+var loggainBtn = document.getElementById("logga_in");
