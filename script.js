c2f04afe4c8086b17797a0506fe4f6a16822c3dc
Förnamn Efternamn <dinepost@example.com>
2019-09-09 15:47:43 +0200

    added welcome side and error side

---
+++ script.js
@@ -0,0 +1,38 @@
+var loggainBlock = document.getElementById("loggain_block");
+var userName = document.getElementById("user_name");
+var pswrd = document.getElementById("pswrd");
+const namn = "test";
+const lsnord = "1234";
+var loggainBtn = document.getElementById("logga_in");
+var loggautBlock = document.getElementById("loggaut_block");
+var loggautBtn = document.getElementById("logga_ut");
+var felBlock = document.getElementById("fel");
+
+loggainBtn.addEventListener("click",function(){
+    if(userName.value==namn && pswrd.value==lsnord)
+    {
+      localStorage.setItem("userName",userName.value);
+      localStorage.setItem("pswrd",pswrd.value);
+      userName.value="";
+      pswrd.value="";
+      loggainBlock.style.display="none";
+      loggautBlock.style.display="block";
+      
+    }
+    else
+    {
+      loggainBlock.style.display="none";
+      loggautBlock.style.display="none";
+      felBlock.style.display="block";
+    }
+  })
+  loggautBtn.addEventListener("click",function()
+  { localStorage.clear();
+    userName.value="";
+    pswrd.value="";
+    loggainBlock.style.display="block";
+    loggautBlock.style.display="none";
+    felBlock.style.display="none";
+  })
+      
+  console.log(localStorage);
